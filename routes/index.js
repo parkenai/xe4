cf174bc6f35b8eef6f8d7b52cf5292a5bde12d0b
pakeenai <pakeenai169@gmail.com>
2019-07-14 10:31:00 +0700

    ไฟล์แก้1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'เกี่ยวกับ' });
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'เกี่ยวกับ' });
+});
+
+router.get('/create', function (req, res, next) {
+  res.render('create', { title: 'ติดต่อเรา' });
 });
 
 module.exports = router;
